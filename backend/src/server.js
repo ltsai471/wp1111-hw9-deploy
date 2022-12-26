a754e127991a5b64572aa6dc50b21c12875a73d3
R10725045 <r10725045@ntu.edu.tw>
2022-12-26 16:17:51 +0800

    fix ip filter

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,6 @@ import routes from './routes';
 import path from "path";
 import { IpFilter } from "express-ipfilter";
 
-app.use(IpFilter(["140.112.0.0/16"], {
-  mode: "allow",
-  detectIp: req => req.connection.remoteAddress?.replace("::ffff:", "") ?? ""
-}));
-
 const app = express();
 if (process.env.NODE_ENV === "development") {
     app.use(cors());
@@ -23,6 +18,11 @@ if (process.env.NODE_ENV === "production") {
     app.get("/*", function (req, res) {
         res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
     });
+
+    app.use(IpFilter(["140.112.0.0/16"], {
+        mode: "allow",
+        detectIp: req => req.connection.remoteAddress?.replace("::ffff:", "") ?? ""
+    }));
 }
 
 const port = process.env.PORT || 4000;
