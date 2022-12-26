733dae5598c2e8f50b2386e39734e07c1b43eb3d
R10725045 <r10725045@ntu.edu.tw>
2022-12-26 16:46:23 +0800

    debug1

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes';
 import path from "path";
 import { IpFilter } from "express-ipfilter";
 
 const app = express();
-if (process.env.NODE_ENV === "development") {
-    app.use(cors());
-}
-
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+// if (process.env.NODE_ENV === "development") {
+//     app.use(cors());
+// }
 app.use('/api', routes);
 
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const API_ROOT =
   process.env.NODE_ENV === "production"
-    ? "/api"
-    : "http://localhost:4000/api";
+    ? "/api/"
+    : "http://localhost:4000/api/";
+
+console.log(API_ROOT)
 
 const instance = axios.create({
   baseURL: API_ROOT,
