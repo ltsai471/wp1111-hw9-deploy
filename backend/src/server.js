4fb578864e90d9e2db3132fb0446c78a70c9b5b7
R10725045 <r10725045@ntu.edu.tw>
2022-12-26 16:13:55 +0800

    fix api url

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,22 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes';
 import path from "path";
+import { IpFilter } from "express-ipfilter";
+
+app.use(IpFilter(["140.112.0.0/16"], {
+  mode: "allow",
+  detectIp: req => req.connection.remoteAddress?.replace("::ffff:", "") ?? ""
+}));
 
 const app = express();
-// const port = process.env.PORT || 4000;
+if (process.env.NODE_ENV === "development") {
+    app.use(cors());
+}
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
-// app.use('/', routes);
+app.use('/api', routes);
 
-// app.listen(port, () =>
-//     console.log(`Example app listening on port ${port}!`),
-// );
+
 if (process.env.NODE_ENV === "production") {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, "../frontend", "build")));
@@ -24,6 +25,7 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-if (process.env.NODE_ENV === "development") {
-    app.use(cors());
-}
+const port = process.env.PORT || 4000;
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`),
+);
